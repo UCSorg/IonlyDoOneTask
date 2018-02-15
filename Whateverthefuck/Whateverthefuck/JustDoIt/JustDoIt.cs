ca8334371a7ec8f399fe45aa824057fb54cbbd03
enymowns <enymowns@gmail.com>
2018-02-15 20:43:13 +0100

    Initial upload
    UCS .Net discord bot for assigning Rank Not Determined role to users without rank and removing it from users with ranks alrdy assigned

---
+++ Whateverthefuck/Whateverthefuck/JustDoIt/JustDoIt.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using System.IO;
+
+namespace Whateverthefuck.JustDoIt
+{
+    public class JustDoIt : ModuleBase
+    {
+        [Command("justdoit")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        public async Task justdoit()
+        {
+            var rolex = Context.Guild.Roles.First(x => x.Name == "Rank Not Determined");
+            var usersAll = await Context.Guild.GetUsersAsync();
+            if (rolex != null)
+            {
+                var m = await Context.Channel.SendMessageAsync("Working...");
+                int users = 0;
+                foreach (SocketGuildUser user in usersAll)
+                {
+                    var role = user.Roles.Where(x => x.Name.Contains("Champion") || x.Name.Contains("Diamond") || x.Name.Contains("Platinum") || x.Name.Contains("Gold") || x.Name.Contains("Silver") || x.Name.Contains("Bronze") || x.Name.Contains("Lurker") || x.Name.Contains("Bots"));
+                    if(role.Count() != 0 && user.Roles.Where(y => y.Name == "Rank Not Determined").Count() != 0)
+                    {
+                        await user.RemoveRoleAsync(rolex);
+                        await Task.Delay(2000);
+                        users = users + 1;
+                        await m.ModifyAsync(x => x.Content = $"Working... **{users}** users handled. This might take a while");
+                    }
+                    if(role.Count() == 0 && user.Roles.Where(y=>y.Name == "Rank Not Determined").Count() == 0)
+                    {
+                        await user.AddRoleAsync(rolex);
+                        await Task.Delay(2000);
+                        users = users + 1;
+                        await m.ModifyAsync(x => x.Content = $"Working... **{users}** users handled. This might take a while");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Whateverthefuck/Whateverthefuck/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Net.WebSockets;
+
+namespace Whateverthefuck
+{
+    class Program
+    {
+        public CommandService commands;
+        public DiscordSocketClient client;
+        public IServiceProvider services;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+        {
+            commands = new CommandService();
+            client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                LogLevel = LogSeverity.Verbose,
+                MessageCacheSize = 1000
+            });
+            client.Log += Logger;
+            commands = new CommandService();
+            await InstallCommands();
+            await client.LoginAsync(TokenType.Bot, "MzY1ODIwOTAzNDExNjc5MjMz.DLj4yg.00Ex2 - 5C7i0eFuZW4iAIlzmMuXo");
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            IServiceCollection map = new ServiceCollection();
+
+            services = map.BuildServiceProvider();
+
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            client.MessageReceived += HandleCommand;
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            var context = new CommandContext(client, message);
+
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private static Task Logger(LogMessage message)
+        {
+            var cc = Console.ForegroundColor;
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
+            Console.ForegroundColor = cc;
+            return Task.CompletedTask;
+        }
+    }
+}
